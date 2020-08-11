c2a19dbaea6372063bba4f75889959ce13ba7fcb
PiotrFe <38947052+PiotrFe@users.noreply.github.com>
2020-08-11 15:16:23 +0200

    commit

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,22 +1,29 @@
-const jwt = require('jwt-simple');
-const config = require('../config');
+const jwt = require("jwt-simple");
+const config = require("../config");
 
 const User = require("../models/user");
 
 function tokenForUser(user) {
-    const timestamp = new Date().getTime();
+  const timestamp = new Date().getTime();
 
-    // we can provide any object to encode a web token
-    // convention is to provide "subject" and "issued at time"
-    // subject can be email, user id etc. id better since email may change
-    return jwt.encode({ sub: user._id, iat: timestamp }, config.secret)
+  // we can provide any object to encode a web token
+  // convention is to provide "subject" and "issued at time"
+  // subject can be email, user id etc. id better since email may change
+  return jwt.encode({ sub: user._id, iat: timestamp }, config.secret);
 }
+
+exports.signin = (req, res, next) => {
+  // passport (middleware in router) attached the user found to req
+  res.send({ token: tokenForUser(req.user) });
+};
 
 exports.signup = (req, res, next) => {
   const { email, password } = req.body;
-  
+
   if (!email || !password) {
-      return res.status(422).send({ error: 'You must provide email and password' });
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
   }
 
   User.findOne({ email: email }, (err, existingUser) => {
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,17 +1,42 @@
-const passport = require('passport');
-const User = require ('../models/user');
-const config = require('../config');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require("passport");
+const User = require("../models/user");
+const config = require("../config");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
 
-const jwtOptions = {};
+// Create local strategy
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+  User.findOne({ email: email }, function (err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false);
 
-const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
-    User.findById(payload.sub, function(err, user) {
-        if (err) return done(err, false);
+    user.comparePassword(password, function (err, isMatch) {
+      if (err) return done(err);
+      if (!isMatch) return done(null, false);
 
-        if (user) done(null, user)
-        else done(null, false)
-    })
+      return done(null, user);
+    });
+  });
 });
 
+// Setup options for JWT strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: config.secret,
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
+  User.findById(payload.sub, function (err, user) {
+    if (err) return done(err, false);
+
+    // passport attaches user to req.user; we can refer to it in authentication.js
+    if (user) done(null, user);
+    else done(null, false);
+  });
+});
+
+passport.use(jwtLogin);
+passport.use(localLogin);
